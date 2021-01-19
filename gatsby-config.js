e0269168fc37365bcc8ec483896c0cec71612f56
Gary Storey <garystorey655@gmail.com>
2021-01-18 23:21:04 -0500

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,38 @@
+module.exports = {
+  plugins: [
+    {
+      resolve: "gatsby-source-shopify",
+      options: {
+        shopName: "junkfood.shopify.com",
+        accessToken: "",
+      },
+    },
+    "gatsby-plugin-sass",
+    "gatsby-plugin-sharp",
+    "gatsby-plugin-sitemap",
+    {
+      resolve: "gatsby-plugin-manifest",
+      options: {
+        icon: "src/images/icon.png",
+      },
+    },
+    "gatsby-plugin-mdx",
+    "gatsby-transformer-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "images",
+        path: "./src/images/",
+      },
+      __key: "images",
+    },
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "pages",
+        path: "./src/pages/",
+      },
+      __key: "pages",
+    },
+  ],
+};
