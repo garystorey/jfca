d459fdbb32c8013b09f20f87f8f785a0e02cf2db
Gary Storey <garystorey655@gmail.com>
2021-01-19 02:06:58 -0500

    update redirectUri

--- src/components/DefaultLayout/index.js
+++ src/components/DefaultLayout/index.js
@@ -8,7 +8,7 @@ export const Layout = ({ children }) => {
     <Auth0Provider
       domain={process.env.GATSBY_AUTH0_DOMAIN}
       clientId={process.env.GATSBY_AUTH0_CLIENT_ID}
-      redirectUri={window.location.origin}
+      redirectUri={`/`}
       audience={process.env.GATSBY_AUTH0_AUDIENCE}
       scope={process.env.GATSBY_AUTH0_SCOPE}
     >
