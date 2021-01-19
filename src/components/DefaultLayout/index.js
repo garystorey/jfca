5820b4585b030cbb535c53587eabd00224ebffe5
Gary Storey <garystorey655@gmail.com>
2021-01-19 02:16:18 -0500

    fix redirectUri again

--- src/components/DefaultLayout/index.js
+++ src/components/DefaultLayout/index.js
@@ -8,7 +8,7 @@ export const Layout = ({ children }) => {
     <Auth0Provider
       domain={process.env.GATSBY_AUTH0_DOMAIN}
       clientId={process.env.GATSBY_AUTH0_CLIENT_ID}
-      redirectUri={`https://jfca.netlify.com`}
+      redirectUri={`https://jfca.netlify.app`}
       audience={process.env.GATSBY_AUTH0_AUDIENCE}
       scope={process.env.GATSBY_AUTH0_SCOPE}
     >
